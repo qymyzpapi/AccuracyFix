49ae84e48c5577172f43f859a2d9707abc49920c
qymyzpapi <169079070+qymyzpapi@users.noreply.github.com>
2026-08-08 00:12:08 +0500

    Add speed limit variable to accuracy fix

--- AccuracyFix/AccuracyFix.cpp
+++ AccuracyFix/AccuracyFix.cpp
@@ -5,10 +5,9 @@ CAccuracyFix gAccuracyFix;
 void CAccuracyFix::ServerActivate()
 {	
 	this->m_af_accuracy_all = gAccuracyUtil.CvarRegister("af_accuracy_all", "-1.0");
-
 	this->m_af_distance_all = gAccuracyUtil.CvarRegister("af_distance_all", "-1.0");
-
 	this->m_af_jump_fix = gAccuracyUtil.CvarRegister("af_jump_fix", "0.0");
+	this->m_af_speed_limit_all = gAccuracyUtil.CvarRegister("af_speed_limit_all", "-1.0");
 
 	if (g_ReGameApi)
 	{
@@ -27,11 +26,9 @@ void CAccuracyFix::ServerActivate()
 						if (SlotInfo->weaponName[0u] != '\0')
 						{
 							Q_snprintf(cvarName, sizeof(cvarName), "af_distance_%s", SlotInfo->weaponName);
-
 							this->m_af_distance[WeaponID] = gAccuracyUtil.CvarRegister(cvarName, "8192.0");
 
 							Q_snprintf(cvarName, sizeof(cvarName), "af_accuracy_%s", SlotInfo->weaponName);
-
 							this->m_af_accuracy[WeaponID] = gAccuracyUtil.CvarRegister(cvarName, "9999.0");
 						}
 					}
@@ -71,37 +68,44 @@ void CAccuracyFix::TraceLine(const float* vStart, const float* vEnd, int fNoMons
 						{
 							if ((Player->m_pActiveItem->iItemSlot() == PRIMARY_WEAPON_SLOT) || (Player->m_pActiveItem->iItemSlot() == PISTOL_SLOT))
 							{
-								auto DistanceLimit = this->m_af_distance[Player->m_pActiveItem->m_iId]->value;
-
-								if (this->m_af_distance_all->value > 0)
+								float speedLimit = this->m_af_speed_limit_all->value;
+								
+								float speedSq = (Player->pev->velocity.x * Player->pev->velocity.x) + (Player->pev->velocity.y * Player->pev->velocity.y);
+								
+								if (speedLimit <= 0.0f || speedSq <= (speedLimit * speedLimit))
 								{
-									DistanceLimit = this->m_af_distance_all->value;
-								}
+									auto DistanceLimit = this->m_af_distance[Player->m_pActiveItem->m_iId]->value;
 
-								if (DistanceLimit > 0.0f)
-								{
-									if ((this->m_af_jump_fix->value > 0) || (Player->pev->flags & FL_ONGROUND))
+									if (this->m_af_distance_all->value > 0)
 									{
-										auto trResult = gAccuracyUtil.GetUserAiming(pentToSkip, DistanceLimit);
-	
-										if (!FNullEnt(trResult.pHit))
-										{
-											auto TargetIndex = ENTINDEX(trResult.pHit);
-	
-											if (TargetIndex > 0 && TargetIndex <= gpGlobals->maxClients)
-											{
-												auto fwdVelocity = this->m_af_accuracy[Player->m_pActiveItem->m_iId]->value;
-	
-												if (this->m_af_accuracy_all->value > 0.0f)
-												{
-													fwdVelocity = this->m_af_accuracy_all->value;
-												}
-	
-												g_engfuncs.pfnMakeVectors(pentToSkip->v.v_angle);
-	
-												auto vEndRes = (Vector)vStart + gpGlobals->v_forward * fwdVelocity;
+										DistanceLimit = this->m_af_distance_all->value;
+									}
 
-												g_engfuncs.pfnTraceLine(vStart, vEndRes, fNoMonsters, pentToSkip, ptr);
+									if (DistanceLimit > 0.0f)
+									{
+										if ((this->m_af_jump_fix->value > 0) || (Player->pev->flags & FL_ONGROUND))
+										{
+											auto trResult = gAccuracyUtil.GetUserAiming(pentToSkip, DistanceLimit);
+		
+											if (!FNullEnt(trResult.pHit))
+											{
+												auto TargetIndex = ENTINDEX(trResult.pHit);
+		
+												if (TargetIndex > 0 && TargetIndex <= gpGlobals->maxClients)
+												{
+													auto fwdVelocity = this->m_af_accuracy[Player->m_pActiveItem->m_iId]->value;
+		
+													if (this->m_af_accuracy_all->value > 0.0f)
+													{
+														fwdVelocity = this->m_af_accuracy_all->value;
+													}
+		
+													g_engfuncs.pfnMakeVectors(pentToSkip->v.v_angle);
+		
+													auto vEndRes = (Vector)vStart + gpGlobals->v_forward * fwdVelocity;
+
+													g_engfuncs.pfnTraceLine(vStart, vEndRes, fNoMonsters, pentToSkip, ptr);
+												}
 											}
 										}
 									}
@@ -114,4 +118,3 @@ void CAccuracyFix::TraceLine(const float* vStart, const float* vEnd, int fNoMons
 		}
 	}
 }
-
