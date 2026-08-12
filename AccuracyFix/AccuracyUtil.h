fc68d58820d27ab0b8ff5dad551e88c3e14df69f
qymyzpapi <169079070+qymyzpapi@users.noreply.github.com>
2026-08-12 16:13:44 +0500

    Refactor GetUserAiming function signature

--- AccuracyFix/AccuracyUtil.h
+++ AccuracyFix/AccuracyUtil.h
@@ -6,9 +6,18 @@ public:
 	cvar_t* CvarRegister(const char* Name, const char* Value);
 	void ServerCommand(const char* Format, ...);
 	const char* GetPath();
-	
-	TraceResult GetUserAiming(edict_t* pEntity, float DistanceLimit, const Vector& vecForward);
-	
+	TraceResult GetUserAiming(edict_t* pEntity, float DistanceLimit);
+
+	// ReHLDS keeps the cvar_t pointer and the cvar name passed to
+	// pfnCVarRegister. Both therefore have to remain valid for the entire
+	// lifetime of the module.
+	static const int MAX_REGISTERED_CVARS = 4 + ((MAX_WEAPONS + 1) * 2);
+	static const int MAX_CVAR_NAME_LENGTH = 64;
+
+	cvar_t m_CvarData[MAX_REGISTERED_CVARS] = {};
+	char m_CvarNames[MAX_REGISTERED_CVARS][MAX_CVAR_NAME_LENGTH] = {};
+	int m_CvarCount = 0;
+
 	std::string m_Path;
 };
 
